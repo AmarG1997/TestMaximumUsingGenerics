e4d7db1b3d7b4ba77fc88c72a8f988bd73ef95cb
AmarArjunGunjal <amargunjal28@gmail.com>
2020-01-02 17:09:34 +0530

    Refactored : All the three methods to one generic method and find maximum

--- src/main/java/MaxElement.java
+++ src/main/java/MaxElement.java
@@ -1,39 +1,15 @@
 public class MaxElement {
 
 
-    public Integer testMax(Integer a, Integer b, Integer c) {
-        Integer maxValue=a;
+    public <E extends Comparable> E testMax(E a,E b, E c) {
+        E maxValue=a;
         if (b.compareTo(maxValue)>0) {
             maxValue = b;
         }
         if (c.compareTo(maxValue)>0) {
             maxValue = c;
         }
-
         return maxValue;
     }
 
-    public Float testFloatMax( Float a, Float b, Float c) {
-        float maxValue=a;
-        if (b.compareTo(maxValue)>0) {
-            maxValue = b;
-        }
-        if (c.compareTo(maxValue)>0) {
-            maxValue = c;
-        }
-
-        return maxValue;
-    }
-
-    public String testStringMax(String a, String b, String c) {
-        String  maxValue=a;
-        if (b.compareTo(maxValue)>0) {
-            maxValue = b;
-        }
-        if (c.compareTo(maxValue)>0) {
-            maxValue = c;
-        }
-
-        return maxValue;
-    }
 }
--- src/test/java/MaximumNumberTest.java
+++ src/test/java/MaximumNumberTest.java
@@ -26,42 +26,42 @@ public class MaximumNumberTest {
     @Test
     public void whenGivenThreeDoubleNumbers_ifFirstNumberIsMax_shouldReturnFirstNumber() {
         MaxElement maxElement = new MaxElement();
-        float max = maxElement.testFloatMax(9.1f,2.1f,6.1f);
+        float max = maxElement.testMax(9.1f,2.1f,6.1f);
         Assert.assertEquals(9.1f,max,0.0);
     }
 
     @Test
     public void whenGivenThreeDoubleNumbers_ifSecondNumberIsMax_shouldReturnSecondNumber() {
         MaxElement maxElement = new MaxElement();
-        float max = maxElement.testFloatMax(9.1f,12.1f,6.1f);
+        float max = maxElement.testMax(9.1f,12.1f,6.1f);
         Assert.assertEquals(12.1f,max,0.0);
     }
 
     @Test
     public void whenGivenThreeDoubleNumbers_ifThirdNumberIsMax_shouldReturnThirdNumber() {
         MaxElement maxElement = new MaxElement();
-        float max = maxElement.testFloatMax(9.1f,12.1f,16.1f);
+        float max = maxElement.testMax(9.1f,12.1f,16.1f);
         Assert.assertEquals(16.1f,max,0.0);
     }
 
     @Test
     public void whenGivenThreeStringValues_ifFirstValueIsMax_shouldReturnFirstNumber() {
         MaxElement maxElement = new MaxElement();
-        String max = maxElement.testStringMax("Peach","Apple","Banana");
+        String max = maxElement.testMax("Peach","Apple","Banana");
         Assert.assertEquals("Peach",max);
     }
 
     @Test
     public void whenGivenThreeStringValues_ifSecondValueIsMax_shouldReturnSecondValue() {
         MaxElement maxElement = new MaxElement();
-        String max = maxElement.testStringMax("Apple","Peach","Banana");
+        String max = maxElement.testMax("Apple","Peach","Banana");
         Assert.assertEquals("Peach",max);
     }
 
     @Test
     public void whenGivenThreeStringValues_ifThirdValueIsMax_shouldReturnThirdValue() {
         MaxElement maxElement = new MaxElement();
-        String max = maxElement.testStringMax("Banana","Apple","Peach");
+        String max = maxElement.testMax("Banana","Apple","Peach");
         Assert.assertEquals("Peach",max);
     }
 }
