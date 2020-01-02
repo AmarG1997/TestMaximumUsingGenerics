022e538fd3cb7dcacdcda7fbddb12cbcf9e17702
AmarArjunGunjal <amargunjal28@gmail.com>
2020-01-02 18:24:25 +0530

    Refactored : Create generic class to take 3 variable of generic type

--- src/main/java/MaxElement.java
+++ src/main/java/MaxElement.java
@@ -1,7 +1,18 @@
-public class MaxElement {
+public class MaxElement <E extends  Comparable> {
+    E a;
+    E b;
+    E c;
 
+    public MaxElement(E a, E b, E c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
 
-    public <E extends Comparable> E testMax(E a,E b, E c) {
+    public MaxElement() {
+    }
+
+    public <E extends Comparable> E testMax(E a, E b, E c) {
         E maxValue=a;
         if (b.compareTo(maxValue)>0) {
             maxValue = b;
@@ -10,6 +21,14 @@ public class MaxElement {
             maxValue = c;
         }
         return maxValue;
+
     }
+
+    public E testMaximum()
+    {
+        E max = testMax(a,b,c);
+        return  max;
+    }
+
 
 }
--- src/test/java/MaximumNumberTest.java
+++ src/test/java/MaximumNumberTest.java
@@ -5,63 +5,70 @@ public class MaximumNumberTest {
     @Test
     public void whenGivenThreeNumber_ifFirstNumberIsMax_shouldReturnFirstNumber() {
         MaxElement maxElement = new MaxElement();
-        int max = maxElement.testMax(9,2,6);
+        int max = (int) maxElement.testMax(9,2,6);
         Assert.assertEquals(9,max);
     }
 
     @Test
     public void whenGivenThreeInteger_ifSecondNumberIsMax_shouldReturnSecondNumber() {
         MaxElement maxElement = new MaxElement();
-        int max = maxElement.testMax(9,12,6);
+        int max = (int) maxElement.testMax(9,12,6);
         Assert.assertEquals(12,max);
     }
 
     @Test
     public void whenGivenThreeInteger_ifThirdNumberIsMax_shouldReturnThirdNumber() {
         MaxElement maxElement = new MaxElement();
-        int max = maxElement.testMax(9,12,16);
+        int max = (int) maxElement.testMax(9,12,16);
         Assert.assertEquals(16,max);
     }
 
     @Test
     public void whenGivenThreeDoubleNumbers_ifFirstNumberIsMax_shouldReturnFirstNumber() {
         MaxElement maxElement = new MaxElement();
-        float max = maxElement.testMax(9.1f,2.1f,6.1f);
+        float max = (float) maxElement.testMax(9.1f,2.1f,6.1f);
         Assert.assertEquals(9.1f,max,0.0);
     }
 
     @Test
     public void whenGivenThreeDoubleNumbers_ifSecondNumberIsMax_shouldReturnSecondNumber() {
         MaxElement maxElement = new MaxElement();
-        float max = maxElement.testMax(9.1f,12.1f,6.1f);
+        float max = (float) maxElement.testMax(9.1f,12.1f,6.1f);
         Assert.assertEquals(12.1f,max,0.0);
     }
 
     @Test
     public void whenGivenThreeDoubleNumbers_ifThirdNumberIsMax_shouldReturnThirdNumber() {
         MaxElement maxElement = new MaxElement();
-        float max = maxElement.testMax(9.1f,12.1f,16.1f);
+        float max = (float) maxElement.testMax(9.1f,12.1f,16.1f);
         Assert.assertEquals(16.1f,max,0.0);
     }
 
     @Test
     public void whenGivenThreeStringValues_ifFirstValueIsMax_shouldReturnFirstNumber() {
         MaxElement maxElement = new MaxElement();
-        String max = maxElement.testMax("Peach","Apple","Banana");
+        String max = (String) maxElement.testMax("Peach","Apple","Banana");
         Assert.assertEquals("Peach",max);
     }
 
     @Test
     public void whenGivenThreeStringValues_ifSecondValueIsMax_shouldReturnSecondValue() {
         MaxElement maxElement = new MaxElement();
-        String max = maxElement.testMax("Apple","Peach","Banana");
+        String max = (String) maxElement.testMax("Apple","Peach","Banana");
         Assert.assertEquals("Peach",max);
     }
 
     @Test
     public void whenGivenThreeStringValues_ifThirdValueIsMax_shouldReturnThirdValue() {
         MaxElement maxElement = new MaxElement();
-        String max = maxElement.testMax("Banana","Apple","Peach");
+        String max = (String) maxElement.testMax("Banana","Apple","Peach");
         Assert.assertEquals("Peach",max);
+    }
+
+    @Test
+    public void whenGivenThreeInputFromConstructor_shouldReturnMax_() {
+        MaxElement maxElement = new MaxElement();
+        float max = (float) maxElement.testMax(9.1f,2.1f,6.1f);
+        Assert.assertEquals(9.1f,max,0.0);
     }
 }
