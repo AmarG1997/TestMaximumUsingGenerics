6345e2f12d02f0c6647904ec83ee3bb773062a69
AmarArjunGunjal <amargunjal28@gmail.com>
2020-01-03 10:32:31 +0530

    Added : Method ptintMax

--- src/main/java/MaxElement.java
+++ src/main/java/MaxElement.java
@@ -1,22 +1,25 @@
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class MaxElement <E extends  Comparable> {
 
     public <E extends Comparable> E testMax(E ...values) {
-        //System.out.println("Number of arguments:"+values);
-        E val[]=values;
-        Arrays.sort(val);
-        System.out.println("maximum value is"+val[val.length-1]);
-        return val[val.length-1];
-
-
+        Arrays.sort(values);
+        System.out.println("maximum value is"+values[values.length-1]);
+        return values[values.length-1];
     }
 
     public E testMaximum(E a, E b ,E c)
     {
         E max = testMax(a,b,c);
+        printMax(max);
         return  max;
     }
+
+    public void printMax(E max)
+    {
+        System.out.println("maximum value is"+max);
+    }
+
+
 
 }
