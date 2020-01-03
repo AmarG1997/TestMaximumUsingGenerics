6cdf96e7fc24a0de73556877c7202bc82aa2310e
AmarArjunGunjal <amargunjal28@gmail.com>
2020-01-03 10:47:51 +0530

    Modified : Call printMax method from testMax

--- src/main/java/MaxElement.java
+++ src/main/java/MaxElement.java
@@ -4,17 +4,17 @@ public class MaxElement <E extends  Comparable> {
 
     public <E extends Comparable> E testMax(E ...values) {
         Arrays.sort(values);
+        printMax(values[values.length-1]);
         return values[values.length-1];
     }
 
     public E testMaximum(E a, E b ,E c)
     {
         E max = testMax(a,b,c);
-        printMax(max);
         return  max;
     }
 
-    public void printMax(E max)
+    public < E > void printMax(E max)
     {
         System.out.println("maximum value is"+max);
     }
